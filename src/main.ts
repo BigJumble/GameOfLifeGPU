532e0a7037f4f0208da3d066978eb0cf565d5dab
Eimantas <eima.eima1244@gmail.com>
2024-09-24 11:37:21 +0300

    fixes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 async function main(){
 
     if (!navigator.gpu) {
+        alert("Doesn't work on this browser, try Chrome / Edge / Opera")
         throw new Error("WebGPU not supported on this browser.");
+
     }
 
 
