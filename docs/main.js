b2743d379310f1c2052f317d532d7be210a2bc26
Eimantas <eima.eima1244@gmail.com>
2024-09-24 11:41:09 +0300

    better warning

--- docs/main.js
+++ docs/main.js
@@ -1,7 +1,6 @@
 "use strict";
 async function main() {
     if (!navigator.gpu) {
-        alert("Doesn't work on this browser, try Chrome / Edge / Opera");
         throw new Error("WebGPU not supported on this browser.");
     }
     Camera.init();
